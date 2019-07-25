2f5c9468bc05d2f394407f74d66b793ead8608a4
张伟江 <zhangwj@tfinfo.cn>
2019-07-25 11:05:43 +0800

    返回按钮修改

---
+++ src/views/fab/eqpmodel/eqpmodelList.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="app-container calendar-list-container">
+    <eap-table v-bind="table" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Eqpmodel',
+  data() {
+    return {
+      table: {
+        url: '/fab/fabequipmentmodel',
+        handler: {
+          create: 'views/fab/eqpmodel/eqpmodelAdd',
+          update: 'views/fab/eqpmodel/eqpmodelEdit'
+          // delete: true,
+          // search: true,
+          // batchDelete: true
+        },
+        sort: 'updateDate',
+        columns: [
+          {
+            name: 'manufacturerName',
+            align: 'center',
+            label: '设备厂家',
+            'formatter.handler': 'update',
+            query: 'true',
+            queryMode: 'input',
+            condition: 'like'
+          },
+          {
+            name: 'classCode',
+            label: '设备类型',
+            query: 'true',
+            queryMode: 'input',
+            condition: 'like'
+          },
+          {
+            name: 'activeFlag',
+            label: '有效标志',
+            'formatter.dict': 'ACTIVE_FLAG'
+          },
+          {
+            name: 'updateDate',
+            label: '更新时间'
+          }
+        ]
+      }
+    }
+  }
+}
+</script>

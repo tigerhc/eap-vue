705e80b2035af337edcc80a23648a45271a5dbc7
张伟江 <zhangwj@tfinfo.cn>
2019-08-21 21:04:50 +0800

    新增设备类型look下设备类型modelname字段

--- src/views/fab/eqpmodel/eqpmodelLook.vue
+++ src/views/fab/eqpmodel/eqpmodelLook.vue
@@ -4,6 +4,7 @@
       <w-table-col name="manufacturerName" label="设备厂家" width="150" query condition="like"/>
       <w-table-col name="classCode" label="设备类型" width="150" query condition="like"/>
       <w-table-col name="activeFlag"  label="有效标志" width="100" dict="ACTIVE_FLAG" />
+      <w-table-col name="modelName" label="设备类型全称"  width="100" formatter="combineModelName"/>
     </w-table>
   </div>
 </template>
@@ -18,7 +19,10 @@
       }
     },
     methods: {
-
+      combineModelName(row, table) {
+        alert(row);
+        return row.manufacturerName+"-"+row.classCode
+      },
     }
   }
 </script>

8c2c1ea09d03f9161b523ec892182311bf738169
张伟江 <zhangwj@tfinfo.cn>
2019-08-26 14:17:46 +0800

    排序测试

--- src/views/fab/eqp/fabequipmentList.vue
+++ src/views/fab/eqp/fabequipmentList.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="app-container calendar-list-container">
-    <w-table v-bind="table" sort="sortCode.asc" >
+    <w-table v-bind="table" url="/fab/fabequipment" sort="sortCode.asc" >
       <!--todo fixed属性导致当前列变为第一列-->
-      <w-table-col name="eqpId" label="设备号" sort="2" fixed align="left" handler="view" query condition="like"/>
+      <w-table-col name="eqpId" label="设备号" sort fixed align="left" handler="view" query condition="like"/>
       <w-table-col name="officeName" label="部门" align="left"/>
       <w-table-col name="modelName" label="设备型号" align="left" />
       <w-table-col name="bcCode" label="BC号" align="center" />
@@ -15,7 +15,7 @@
       <!--<w-table-col name="updateDate" label="更新时间" width="200" align="center" sort="1" query querymode="date" condition="between"/>-->
 
       <!--hidden属性: 隐藏默认button url: 修改默认url 没有url,则默认调用属性name值的方法-->
-      <!--<w-table-toolbar name="add" hidden url="views/fab/eqpmodel/eqpmodelAdd" />-->
+      <w-table-toolbar name="add" url="views/fab/eqp/fabequipmentEdit" />
       <!--hidden属性: 隐藏默认button url: 修改默认url-->
       <!--<w-table-toolbar name="exportExcel" label="导出Excel" tip="你想干啥111？" icon="fa-download" type="success" />-->
       <w-table-button name="stop" label="停用设备" tip="确认停用设备？" icon="el-icon-close" />
@@ -32,26 +32,6 @@ export default {
   data() {
     return {
       table: {
-        url: '/fab/fabequipment',
-        handler: {
-          add: 'views/fab/eqp/fabequipmentEdit',
-          // edit: 'views/fab/eqpmodel/fabequipmentEdit',
-          // view: 'views/fab/eqpmodel/fabequipmentEdit'
-          // delete: true,
-          // search: true,
-          batchDelete: true
-        }
-        //        sort: 'updateDate'
-        //          columns: [
-        //            {name: 'eqpId',            label: '设备号',       align: 'left',  'formatter.handler': 'view', query: 'true', queryMode: 'input', condition: 'like'},
-        //            {name: 'officeName',     label: '部门',         align: 'left'  },
-        //            {name: 'bcCode',           label: 'BC号',         align: 'center' , query: 'true', queryMode: 'input', condition: 'like'},
-        //            {name: 'ip',               label: '机台IP地址',   align: 'center'},
-        //            {name: 'modelName',        label: '设备型号名称', align: 'left', query: 'true', queryMode: 'input', condition: 'like'},
-        //            {name: 'location',         label: '位置号',       align: 'center'},
-        //            {name: 'activeFlag',       label: '有效标志',     align: 'center', 'formatter.dict': 'ACTIVE_FLAG'},
-        //            {name: 'updateDate',       label: '更新时间',     align: 'center'}
-        //          ]
       }
     }
   },
@@ -62,7 +42,7 @@ export default {
         url: '/fab/fabequipment/' + row.id + '/inactiveeqp',
         method: 'post'
       }).then(() => {
-        //        this.getList()
+        // ctx.refresh()
         this.$notify({
           title: '成功',
           message: '已禁用',

01b9ddd728e27d556abcbfb97a3ffbcaf4a98e5f
tortozhang@gmail.com <zhangwj@tfinfo.cn>
2020-04-13 11:42:36 +0800

    登陆界面样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,20 +2,20 @@
   <div class="login-container">
     <div class="login-main">
       <div class="login-introduce">
-        <div class="title" style="color:#fff">云端软件开发协作平台</div>
-        <div class="content" style="color:#fff">
-          王安 DCloud CEO不少国内的好产品，因为用户的迷外心理而不被重视。其实一个友好的中文界面，可以让用户快速掌握软件的所有功能，实实在在地提升效率。期待更多开发者能重视 Gitee，重视国内认真做事的产品团队，他们会给你惊喜。logo
-          云端软件开发协作平王安 DCloud CEO不少国内的好产品，因为用户的迷外心理而不被重视。其实一个友好的中文界面，可以让用户快速掌握软件的所有功能，实实在在地提升效率。期待更多开发者能重视 Gitee，重视国内认真做事的产品团队，他们会给你惊喜。logo
-          云端软件开发协作平王安 DCloud CEO不少国内的好产品，因为用户的迷外心理而不被重视。其实一个友好的中文界面，可以让用户快速掌握软件的所有功能，实实在在地提升效率。期待更多开发者能重视 Gitee，重视国内认真做事的产品团队，他们会给你惊喜。logo
-          云端软件开发协作平
-
-        </div>
+        <div class="title" style="color:#fff">DSK CIM</div>
+        <div class="content" style="color:#fff">DSK CIM</div>
       </div>
-      <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
-
+      <el-form
+        ref="loginForm"
+        :model="loginForm"
+        :rules="loginRules"
+        class="login-form"
+        auto-complete="on"
+        label-position="left"
+      >
         <div class="title-container">
           <h3 class="title">{{ $t('login.title') }}</h3>
-          <lang-select class="set-language"/>
+          <lang-select class="set-language" />
         </div>
 
         <el-form-item prop="username">
@@ -41,13 +41,19 @@
             :placeholder="$t('login.password')"
             name="password"
             auto-complete="on"
-            @keyup.enter.native="handleLogin" />
+            @keyup.enter.native="handleLogin"
+          />
           <span class="show-pwd" @click="showPwd">
             <svg-icon icon-class="eye" />
           </span>
         </el-form-item>
 
-        <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">{{ $t('login.logIn') }}</el-button>
+        <el-button
+          :loading="loading"
+          type="primary"
+          style="width:100%;margin-bottom:30px;"
+          @click.native.prevent="handleLogin"
+        >{{ $t('login.logIn') }}</el-button>
 
         <div class="tips">
           <span>{{ $t('login.username') }} : admin</span>
@@ -55,7 +61,6 @@
         </div>
       </el-form>
     </div>
-
   </div>
 </template>
 
@@ -104,7 +109,6 @@ export default {
       },
       immediate: true
     }
-
   },
   created() {
     // window.addEventListener('hashchange', this.afterQRScan)
@@ -121,20 +125,23 @@ export default {
       }
     },
     handleLogin() {
-      this.$refs.loginForm.validate(valid => {
+      this.$refs.loginForm.validate((valid) => {
         if (valid) {
           this.loading = true
-          this.$store.dispatch('LoginByUsername', this.loginForm).then(() => {
-            this.loading = false
-            this.$router.push({ path: 'dashboard' || '/' })
-          }).catch((error) => {
-            this.loading = false
-            Message({
-              message: error,
-              type: 'error',
-              duration: 5 * 1000
+          this.$store
+            .dispatch('LoginByUsername', this.loginForm)
+            .then(() => {
+              this.loading = false
+              this.$router.push({ path: 'dashboard' || '/' })
             })
-          })
+            .catch((error) => {
+              this.loading = false
+              Message({
+                message: error,
+                type: 'error',
+                duration: 5 * 1000
+              })
+            })
         } else {
           console.log('error submit!!')
           return false
@@ -164,63 +171,62 @@ export default {
 </script>
 
 <style rel="stylesheet/scss" lang="scss">
-  /* 修复input 背景不协调 和光标变色 */
-  /* Detail see https://gitee.com/dataact/jeeweb/pull/927 */
+/* 修复input 背景不协调 和光标变色 */
+/* Detail see https://gitee.com/dataact/jeeweb/pull/927 */
 
-  $bg:#283443;
-  $light_gray:#eee;
-  $cursor: #fff;
+$bg: #283443;
+$light_gray: #eee;
+$cursor: #fff;
 
-  @supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
-    .login-container .el-input input{
-      color: $cursor;
-      &::first-line {
-        color: $light_gray;
-      }
+@supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
+  .login-container .el-input input {
+    color: $cursor;
+    &::first-line {
+      color: $light_gray;
     }
   }
+}
 
-  /* reset element-ui css */
-  .login-container {
-    .el-input {
-      display: inline-block;
+/* reset element-ui css */
+.login-container {
+  .el-input {
+    display: inline-block;
+    height: 47px;
+    width: 85%;
+    input {
+      background: transparent;
+      border: 0px;
+      -webkit-appearance: none;
+      border-radius: 0px;
+      padding: 12px 5px 12px 15px;
+      color: $light_gray;
       height: 47px;
-      width: 85%;
-      input {
-        background: transparent;
-        border: 0px;
-        -webkit-appearance: none;
-        border-radius: 0px;
-        padding: 12px 5px 12px 15px;
-        color: $light_gray;
-        height: 47px;
-        caret-color: $cursor;
-        &:-webkit-autofill {
-          -webkit-box-shadow: 0 0 0px 1000px $bg inset !important;
-          -webkit-text-fill-color: $cursor !important;
-        }
+      caret-color: $cursor;
+      &:-webkit-autofill {
+        -webkit-box-shadow: 0 0 0px 1000px $bg inset !important;
+        -webkit-text-fill-color: $cursor !important;
       }
     }
-    .el-form-item {
-      border: 1px solid rgba(255, 255, 255, 0.1);
-      background: rgba(0, 0, 0, 0.1);
-      border-radius: 5px;
-      color: #454545;
-    }
   }
+  .el-form-item {
+    border: 1px solid rgba(255, 255, 255, 0.1);
+    background: rgba(0, 0, 0, 0.1);
+    border-radius: 5px;
+    color: #454545;
+  }
+}
 </style>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
-$bg:#2d3a4b;
-$dark_gray:#889aa4;
-$light_gray:#eee;
+$bg: #2d3a4b;
+$dark_gray: #889aa4;
+$light_gray: #eee;
 
 .login-container {
-
   // filter:  opacity(50%);
   // -webkit-filter: opacity(50%);
   // background-color: #fff;
-  background-attachment:fixed;
+  background-attachment: fixed;
   position: fixed;
   height: 100%;
   width: 100%;
@@ -240,21 +246,48 @@ $light_gray:#eee;
       width: 100%;
       color: #000;
       text-align: center;
-      text-indent:2em;
+      text-indent: 2em;
       // height: 50%;
-      // background-color: green;
-      background:url(../../assets/img/beijing1.jpg) no-repeat;
+      // background-color: linear-gradient(0deg, #3a485a 0%, #607089 100%);
+
+      background: linear-gradient(0deg, #3a485a 0%, #607089 100%);
       // filter:  opacity(50%);
       // -webkit-filter: opacity(50%);
       // opacity: 0.9;
-      background-size:100% 100%;
+      background-size: 100% 100%;
       padding: 20px;
+      position: relative;
+      &::before {
+        position: absolute;
+          top: 0;
+          left: 0;
+          bottom: 0;
+          right: 0;
+        background: url(../../assets/img/left-1.png) no-repeat 0 0;
+      }
+      &::after {
+        content: '';
+          position: absolute;
+          top: 0;
+          left: 0;
+          bottom: 0;
+          right: 0;
+        background: url(../../assets/img/left-2.png) no-repeat right bottom;
+      }
       .title {
         margin-top: 20px;
         font-size: 20px;
         padding: 10px;
       }
     }
+
+    // .login-introduce::before{
+    //   background:url(../../assets/img/left-1.png) no-repeat;
+    // }
+
+    // .login-introduce::after{
+    //   background:url(../../assets/img/left-2.png) no-repeat right bottom;
+    // }
     .login-form {
       float: left;
       width: 100%;
@@ -316,24 +349,22 @@ $light_gray:#eee;
       bottom: 28px;
     }
   }
-
 }
 </style>
 <style scoped>
 @media (min-width: 768px) {
-    .login-container .login-introduce {
-      width: 50%!important;
-      height: 100%!important;
-      /* overflow-y: scroll; */
-    }
-    .login-container .login-introduce::-webkit-scrollbar {
-      display:none
-    }
-    .login-container .login-form {
-      width: 50%!important;
-      height: 100%!important;
-    }
+  .login-container .login-introduce {
+    width: 50% !important;
+    height: 100% !important;
+    /* overflow-y: scroll; */
+  }
+  .login-container .login-introduce::-webkit-scrollbar {
+    display: none;
+  }
+  .login-container .login-form {
+    width: 50% !important;
+    height: 100% !important;
+  }
 }
-
 </style>
 

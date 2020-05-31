e9a65489c8940f124f6fa3a039442d18ab59a3f1
JewelKing <364957326@qq.com>
2020-05-31 18:04:38 +0800

    报表页面样式修改

--- src/views/rpt/wip/rtplotwip.vue
+++ src/views/rpt/wip/rtplotwip.vue
@@ -18,24 +18,37 @@ v-for="item in lineNoOptions"
         <el-button type="primary" @click="serch">查询</el-button>
       </el-row>
     </el-form>
-
+    <el-table :data="tableData" style="width: 100%" >
+      <el-table-column v-for="col in cols" :key="col.prop" :fixed="col.fixed" :prop="col.prop" :label="col.label" :class-name="col.color">
+        <el-table-column v-for="(son, k) in col.child" :key="k" :prop="son.prop" :label="son.label" :class-name="son.color"/>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 <script>
-import { rtplotyieldday } from '@/api/public'
+import request from '@/utils/request'
 
 export default {
   name: 'Rtplotwip',
   components: {},
   data() {
     return {
+      tableData: [],
+      colsDef: [
+        { 'prop': 'production_name', 'label': '品名' },
+        { 'prop': 'lot_no', 'label': 'NO' }
+      ],
+      cols: [
+        { 'fixed': true, 'prop': 'production_name', 'label': '品名' },
+        { 'fixed': true, 'prop': 'lot_no', 'label': 'NO' }
+      ], // 列集合
+      lineNo: '',
       form: {
         lineNo: undefined
       },
       formRules: {
         lineNo: [{ required: true, message: '请选择线别！', trigger: 'change' }]
       },
-      list: [],
       source: [],
       // 先写死
       lineNoOptions: [{
@@ -43,40 +56,64 @@ export default {
         label: 'SIM'
       }, {
         value: 'SMA',
-        label: 'SMA',
-        disabled: true
+        label: 'SMA'
       }, {
         value: 'SX',
-        label: 'SX',
-        disabled: true
-      }, {
-        value: '5GI',
-        label: '5GI',
-        disabled: true
-      }, {
-        value: '6GI',
-        label: '6GI',
-        disabled: true
+        label: 'SX'
       }]
     }
   },
   mounted() {
+
   },
   created() {
+    this.initStation()
   },
   methods: {
+    initStation() {
+      request({
+        url: '/sys/organization/findStep',
+        method: 'get'
+      }).then((response) => {
+        const stations = response.data
+        const c = { 'prop': '', 'label': '工程一览' }
+        const child = []
+        for (let i = 0; i < stations.length; i++) {
+          child.push({ prop: stations[i].name, label: stations[i].name, color: '' })
+        }
+        c.child = child
+        this.cols.push(c)
+      })
+    },
     serch() {
-      this.$refs['form'].validate((valid) => {
-        if (valid) {
-          rtplotyieldday({
-            lineNo: this.form.lineNo
-          }).then((res) => {
-            const data = res.data
-            this.source = data.yield
-            this.initChart()
-          })
+      request({
+        url: '/edc/rptlotyield/findLotYield?lineNo=' + this.form.lineNo,
+        method: 'get'
+      }).then((response) => {
+        console.log(response)
+        const data = response.data.yield
+        for (let i = 0; i < data.length; i++) {
+          for (let j = 0; j < this.cols[2].child.length; j++) {
+            this.cols[2].child[j].color = 'jk-blue'
+            if (this.cols[2].child[j].prop === data[i].step_id) {
+              this.$set(data[i], this.cols[2].child[j].prop, data[i].lot_yield)
+              break
+            }
+          }
+          this.tableData.push(data[i])
         }
       })
+      // this.$refs['form'].validate((valid) => {
+      //   if (valid) {
+      //     rtplotyieldday({
+      //       lineNo: this.form.lineNo
+      //     }).then((res) => {
+      //       const data = res.data
+      //       this.source = data.yield
+      //       console.log(res)
+      //     })
+      //   }
+      // })
     }
   }
 }
@@ -90,7 +127,7 @@ export default {
     .form {
       margin-top: 20px;
     }
+
   }
-</style>
-<style>
+
 </style>

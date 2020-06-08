6cfb3722d18080cdcf89dce196e7734450e28271
JewelKing <364957326@qq.com>
2020-06-08 18:00:07 +0800

    单行数据

--- src/views/ms/record/measurerecordEdit.vue
+++ src/views/ms/record/measurerecordEdit.vue
@@ -1,17 +1,35 @@
 <template>
-  <w-form v-bind="formConf" :col="3" :model="model">
-    <el-input v-model="model.recordId" label="流水号" />
-    <el-input v-model="model.eqpId" label="设备号" />
-    <el-input v-model="model.lotNo" label="批号" />
-    <el-input v-model="model.waferId" label="晶圆ID" />
-    <el-input v-model="model.timing" label="时机" />
-    <w-select-dept v-model="model.sampleCount" label="采样数" />
-    <el-input v-model="model.status" label="状态" />
-    <el-input v-model="model.approveResult" label="判定结果" />
-    <el-row col="24" />
-    <el-input v-model="model.createByName" :disabled="true" label="创建人" />
-    <el-input v-model="model.createDate" :disabled="true" label="创建日期" />
-  </w-form>
+  <div>
+    <w-form v-bind="formConf" :col="3" :model="model">
+      <el-input v-model="model.recordId" label="流水号" />
+      <el-input v-model="model.eqpId" label="设备号" />
+      <el-input v-model="model.lotNo" label="批号" />
+      <el-input v-model="model.waferId" label="晶圆ID" />
+      <el-input v-model="model.timing" label="时机" />
+<!--      <w-select-dept v-model="model.sampleCount" label="采样数" />-->
+      <el-input v-model="model.status" label="状态" />
+      <el-input v-model="model.approveResult" label="判定结果" />
+      <el-row col="24" />
+      <el-input v-model="model.createByName" :disabled="true" label="创建人" />
+      <el-input v-model="model.createDate" :disabled="true" label="创建日期" />
+    </w-form>
+    <el-form v-if="rowData.length > 0" :model="model" disabled="true">
+      <div v-for="(item,index) in rowData" :key="item.id">
+        <el-row v-if="index%2==0">
+          <el-col :xs="24" :span="8">
+            <el-form-item :label="item.itemName" label-width="150px">
+              <el-input v-model="item.itemValue"/>
+            </el-form-item>
+          </el-col>
+          <el-col v-if="index < rowData.length" :xs="24" :span="8">
+            <el-form-item :label="rowData[index+1].itemName" label-width="150px">
+              <el-input v-model="rowData[index+1].itemValue"/>
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </div>
+    </el-form>
+  </div>
 </template>
 <script>
 export default {
@@ -20,6 +38,7 @@ export default {
     return {
       model: {
         eqpId: '',
+        detail: [],
         officeIds: [],
         bcCode: '',
         ip: '',
@@ -31,7 +50,33 @@ export default {
         officeId: '',
         projectId: '2'
       },
-
+      rowData: [{
+        'delFlag': '0',
+        'id': '111',
+        'itemName': 'Range',
+        'itemResult': 'Y',
+        'itemValue': '10.1875',
+        'limitMax': '15',
+        'limitMin': '6',
+        'msRecordId': '41ac7f6818654f6eb48de5b96f31aafd',
+        'sampleNo': 1,
+        'showType': 'input',
+        'sortNo': 1
+      }, {
+        'delFlag': '0',
+        'id': '222',
+        'itemName': 'Points Tested',
+        'itemResult': 'Y',
+        'itemValue': '37',
+        'limitMax': '10',
+        'limitMin': '',
+        'limitType': '',
+        'msRecordId': '41ac7f6818654f6eb48de5b96f31aafd',
+        'sampleNo': 1,
+        'showType': 'input',
+        'sortNo': 2
+      }],
+      gridData: [],
       formConf: {
         url: '/ms/msmeasurerecord/',
         title: {
@@ -43,19 +88,29 @@ export default {
           eqpId: [{ required: true, message: '设备号必填', trigger: 'blur' }],
           modelName: [{ required: true, message: '设备类型必填', trigger: ['blur', 'change'] }],
           activeFlag: [{ required: true, message: '有效标志必选', trigger: 'change' }]
+        },
+        onLoadData: (m, type) => {
+          console.info(m)
+          // m.officeIds = m.officeIds.split(',')
+          // this.rowData = []
+          this.gridData = []
+          if (m.detail) {
+            for (let i = 0; i < m.detail.length; i++) {
+              if (m.detail[i].showType === 'input') {
+                // this.rowData.push(m.detail[i])
+              } else if (m.detail[i].showType === 'grid') {
+                this.gridData.push(m.detail[i])
+              }
+            }
+          }
+          return m
+        },
+        beforeSubmit: (params, type) => {
+          const re = { ...params }
+          // re.officeId = re.officeIds[re.officeIds.length - 1]
+          // re.officeIds = undefined
+          return re
         }
-        // ,
-        // onLoadData: (m, type) => {
-        //   console.info(m)
-        //   m.officeIds = m.officeIds.split(',')
-        //   return m
-        // },
-        // beforeSubmit: (params, type) => {
-        //   const re = { ...params }
-        //   re.officeId = re.officeIds[re.officeIds.length - 1]
-        //   re.officeIds = undefined
-        //   return re
-        // }
       }
     }
   },

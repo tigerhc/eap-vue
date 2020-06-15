ae7fa894b104fe840617e3f7a8e64bb3dbf8ce34
tortozhang@gmail.com <zhangwj@tfinfo.cn>
2020-06-15 08:19:24 +0800

    量测设备类型,去除修改时间查询.

--- src/views/ms/eqpablility/mseqpablilityList.vue
+++ src/views/ms/eqpablility/mseqpablilityList.vue
@@ -4,15 +4,7 @@
       <w-table-col name="manufacturerName" label="设备厂家" width="300" query condition="like" fixed handler="view"/>
       <w-table-col name="classCode" label="设备类型" width="300" query condition="like" />
       <w-table-col name="activeFlag" label="有效标志" width="200" dict="ACTIVE_FLAG" />
-      <w-table-col
-        name="updateDate"
-        label="更新时间"
-        width="200"
-        sort
-        query
-        querymode="date"
-        condition="between"
-      />
+      <w-table-col name="updateDate" label="更新时间" width="200" sort />
       <w-table-toolbar name="add" url="views/ms/eqpablility/mseqpablilityEdit" />
       <w-table-toolbar name="batchDelete" hidden/>
       <w-table-button name="delete" hidden/>

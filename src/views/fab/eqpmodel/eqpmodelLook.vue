4db5560d67d812b3c71a1a0343903eae19cd74e5
JewelKing <364957326@qq.com>
2020-06-23 09:34:31 +0800

    插件module修改

--- src/views/fab/eqpmodel/eqpmodelLook.vue
+++ src/views/fab/eqpmodel/eqpmodelLook.vue
@@ -3,28 +3,28 @@
     <w-table url="/fab/fabequipmentmodel">
       <w-table-col name="manufacturerName" label="设备厂家" width="150" query condition="like"/>
       <w-table-col name="classCode" label="设备类型" width="150" query condition="like"/>
-      <w-table-col name="activeFlag"  label="有效标志" width="100" dict="ACTIVE_FLAG" />
-      <w-table-col name="modelName" label="设备类型全称"  width="100" formatter="combineModelName"/>
+      <w-table-col name="activeFlag" label="有效标志" width="100" dict="ACTIVE_FLAG" />
+      <w-table-col :formatter="combineModelName" name="modelName" label="设备类型全称" width="100"/>
     </w-table>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'EqpmodelLook',
-    data() {
-      return {
-        table: {
-        }
+export default {
+  name: 'EqpmodelLook',
+  data() {
+    return {
+      table: {
       }
-    },
-    methods: {
-      combineModelName(row, table) {
-        alert(row);
-        return row.manufacturerName+"-"+row.classCode
-      },
+    }
+  },
+  methods: {
+    combineModelName(val, col, table, row) {
+      // alert(row)
+      return row.manufacturerName + '-' + row.classCode
     }
   }
+}
 </script>
 <style>
 </style>

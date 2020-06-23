aadcc914597edeea1f5ce5f4bd1af5514124cde0
忧伤灬看不透 <hsiguo@163.com>
2020-06-23 14:58:42 +0800

    添加功能信息界面

---
+++ src/views/fab/eqp/sysfunctionEdit.vue
@@ -0,0 +1,45 @@
+<template>
+  <w-form v-bind="formConf" :col="3" :model="model">
+    <el-input v-model="model.fctType" align="center" label="功能类型" />
+    <el-input v-model="model.fctCode" label="功能代码" />
+    <el-input v-model="model.fctDesc" label="功能描述" />
+    <w-select-dic v-model="model.activeFlag" style="width:100%" label="有效标志" dict="ACTIVE_FLAG" />
+    <el-input v-model="model.updateByName" :disabled="true" label="更新人" />
+    <el-input v-model="model.updateDate" :disabled="true" label="更新日期" />
+  </w-form>
+</template>
+<script>
+export default {
+  name: 'SysFunctionEdit',
+  data() {
+    return {
+      model: {
+        fctType: '',
+        fctCode: '',
+        fctDesc: '',
+        activeFlag: ''
+      },
+
+      formConf: {
+        url: '/core/sysfunction/',
+        title: {
+          ADD: '新增功能',
+          EDIT: '修改功能',
+          VIEW: '功能详情'
+        },
+        rules: {
+          activeFlag: [{ required: true, message: '有效标志必选', trigger: 'change' }]
+        },
+        onLoadData: (m, type) => {
+          console.info(m)
+          return m
+        },
+        beforeSubmit: (params, type) => {
+          const re = { ...params }
+          return re
+        }
+      }
+    }
+  }
+}
+</script>
---
+++ src/views/fab/eqp/sysfunctionList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="app-container calendar-list-container">
+
+    <w-table v-slot="{row}" v-bind="table" url="/core/sysfunction" sort="create_date.desc" >
+      <!--todo fixed属性导致当前列变为第一列-->
+      <w-table-col name="fctType" label="功能类型" align="left" handler="view" query condition="like"/>
+      <w-table-col name="fctCode" label="功能代码" width="400" align="left"/>
+      <w-table-col name="fctDesc" label="功能描述" width="300" align="left" query condition="like"/>
+      <w-table-col name="activeFlag" label="有效标志" width="150" align="center" dict="ACTIVE_FLAG" query condition="eq"/>
+      <w-table-col name="createDate" label="更新时间" sort width="200" align="center" query querymode="date" condition="between"/>
+
+      <w-table-toolbar name="add" url="views/fab/eqp/sysfunctionEdit" />
+
+    </w-table>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SysFunctionList',
+  data() {
+    return {
+      table: {
+      }
+    }
+  }
+}
+</script>

f7a64683b172d6cdbb595b94f9384f9be469989d
zhangwj <zhangwj@tfinfo.cn>
2020-08-13 10:01:17 +0800

    界面样式微调

--- src/views/dsk/operation/logoperationList.vue
+++ src/views/dsk/operation/logoperationList.vue
@@ -3,10 +3,15 @@
     <w-table v-bind="table" url="/dsk/edcdsklogoperation" sort="startTime.desc">
       <w-table-col name="eqpId" label="设备号" sort fixed align="left" handler="view" query dict multiple eqp condition="in"/>
       <w-table-col name="eqpModelName" label="设备型号" align="left"/>
+      <w-table-col name="eqpModelId" label="设备型号" hidden query dict url="/fab/fabequipmentmodel/list" namekey="modelName" condition="eq" filterable/>
+      <w-table-col name="eventId" label="事件ID" align="left" query condition="like"/>
+      <w-table-col name="alarmCode" label="报警ID" align="left"/>
+      <w-table-col name="eventName" label="事件内容" align="left"/>
+      <w-table-col name="eventDetail" label="事件详细" align="left"/>
+
       <w-table-col name="recipeCode" label="配方名" align="center"/>
       <w-table-col name="dayYield" label="日产量" align="right"/>
       <w-table-col name="lotYield" label="批次产量" align="right"/>
-      <w-table-col name="eqpModelId" label="设备型号" hidden query dict url="/fab/fabequipmentmodel/list" namekey="modelName" condition="eq" filterable/>
       <w-table-col name="startTime" label="发生时间" width="200" align="center" sort="1" query querymode="date" condition="between"/>
       <w-table-toolbar name="add" url="views/dsk/operation/logoperationEdit" hidden/>
       <w-table-toolbar name="batchDelete" hidden/>

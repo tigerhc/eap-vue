99ce89e72aa7c1414763c79574c738c1b02266f8
zhangwj <zhangwj@tfinfo.cn>
2020-08-13 23:19:07 +0800

    长度修改

--- src/views/rms/recipe/rmsrecipeList.vue
+++ src/views/rms/recipe/rmsrecipeList.vue
@@ -2,20 +2,20 @@
   <div class="app-container calendar-list-container">
     <w-table v-slot="{row}" v-bind="table" url="rms/rmsrecipe/" sort="updateDate.desc, createDate.desc" >
       <!--todo fixed属性导致当前列变为第一列-->
-      <w-table-col name="recipeCode" label="程序名称" sort fixed align="center" handler="view" query condition="like"/>
-      <w-table-col name="eqpId" label="设备号" sort fixed align="center" query dict multiple eqp condition="in"/>
+      <w-table-col name="recipeCode" label="程序名称" width="180" sort fixed align="left" handler="view" query condition="like"/>
+      <w-table-col name="eqpId" label="设备号" width="150" sort fixed align="center" query dict multiple eqp condition="in"/>
       <w-table-col name="eqpModelName" width="200" label="设备类型" align="center" />
       <w-table-col name="eqpModelId" label="设备型号ID" hidden dict query url="/fab/fabequipmentmodel/list" namekey="modelName" condition="eq" filterable />
       <w-table-col name="versionType" label="程序等级" align="center" dict="RECIPE_VERSION_TYPE" query condition="eq" filterable />
-      <w-table-col name="versionNo" label="程序版本号" align="center"/>
+      <w-table-col name="versionNo" label="版本号" align="center"/>
       <w-table-col name="status" label="状态" dict="RECIPE_STATUS" query condition="eq" filterable />
-      <w-table-col name="approveStep" label="审核状态" align="center" dict="RECIPE_APPROVE_STEP" query condition="eq" filterable />
+      <w-table-col name="approveStep" label="审核状态" width="100" align="center" dict="RECIPE_APPROVE_STEP" query condition="eq" filterable />
       <w-table-col name="approveResult" label="审核结果" width="100" align="center" dict="RECIPE_APPROVE_RESULT" query condition="eq" filterable foldcolor/>
-      <w-table-col name="createDate" label="创建时间" width="200" align="center"/>
+      <w-table-col name="createDate" label="创建时间" width="180" align="center"/>
       <w-table-col name="createByName" label="上传人" align="center" query condition="eq" />
       <!--<w-table-col name="versionNo" label="程序版本号" align="center"/>-->
       <!--<w-table-col name="versionNo" label="程序版本号" align="center"/>-->
-      <w-table-col name="updateDate" label="更新时间" width="200" align="center" sort="1" query querymode="date" condition="between"/>
+      <w-table-col name="updateDate" label="更新时间" width="180" align="center" sort="1" query querymode="date" condition="between"/>
 
       <!--hidden属性: 隐藏默认button url: 修改默认url 没有url,则默认调用属性name值的方法-->
       <w-table-toolbar name="add" hidden url="views/rms/recipe/rmsrecipeEdit" />

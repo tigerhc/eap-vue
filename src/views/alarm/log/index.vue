4e21745867167dcd25ab9861a451ce259dadd619
jiafuxing <1640158339@qq.com>
2020-11-19 13:19:47 +0800

    bug修复

--- src/views/alarm/log/index.vue
+++ src/views/alarm/log/index.vue
@@ -6,8 +6,7 @@
       <w-table-col name="alarmName" label="报警名称" align="left" />
       <w-table-col name="lotNo" label="批次" align="center" width="100" />
       <w-table-col name="lotYield" label="批次产量" width="80" align="center" />
-      <w-table-col name="startDate" label="报警开始时间" width="200" align="center" sort="1" query querymode="date" condition="gt"/>
-      <w-table-col name="endDate" label="报警结束时间" width="200" align="center" sort="1" query querymode="date" condition="between"/>
+      <w-table-col name="startDate" label="发生时间" width="200" align="center" sort="1" query querymode="date" condition="between"/>
       <w-table-toolbar name="add" hidden/>
       <w-table-toolbar name="batchDelete" hidden />
       <w-table-button name="delete" hidden />

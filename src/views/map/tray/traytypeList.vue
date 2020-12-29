503285c9825e4772856f2b58dc7312ef4a51ec89
dongle <donglele_27@126.com>
2020-12-29 16:01:05 +0800

    类型调整

--- src/views/map/tray/traytypeList.vue
+++ src/views/map/tray/traytypeList.vue
@@ -5,7 +5,7 @@
 			<w-table-col name="trayModel" label="治具种类" width="100" align="left" query condition="eq"/>
       <w-table-col name="trayRow" label="托盘行" width="100" align="left"/>
       <w-table-col name="trayCol" label="托盘列" width="100" align="left"/>
-			<w-table-col name="trayType" label="治具托盘" width="100" align="left" dict="sf"/>
+			<w-table-col name="trayType" label="治具托盘" width="100" align="left" dict="TRAY_TYPE"/>
 			<w-table-col name="remarks" label="描述" width="100" align="left"/>
       <w-table-toolbar hidden name="batchDelete" />
       <w-table-toolbar name="add" url="views/map/tray/traytypeEdit" />

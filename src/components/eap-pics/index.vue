5339feef243b4f3077114a109937b1ecfa1df17e
jiafuxing <1640158339@qq.com>
2021-01-26 14:18:36 +0800

    组件生成

---
+++ src/components/eap-pics/index.vue
@@ -0,0 +1,33 @@
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: 'WSelectPic',
+  props: {
+    url: {
+      type: String,
+      default: ''
+    },
+    list1: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  computed: {
+    ...mapGetters(['name']),
+    $p() {
+      return require('@img/' + this.url + '.png')
+    } },
+  render: function(h) {
+    return h('div', { style: { backgroundImage: 'url(' + this.$p + ')' }, attrs: { id: 'id' }}, this.list1.map((t, id) => h('div', { style: t }, t['id'])))
+  }
+}
+</script>
+<style lang="scss" scoped>
+  @import "src/styles/proindex";
+  .id{
+    width: 100%;
+    height:100%;
+  }
+</style>

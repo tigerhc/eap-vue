6528b5472509b40250b06beed79bf98bceb50a13
jiafuxing <1640158339@qq.com>
2021-01-27 10:33:51 +0800

    优化

--- src/components/eap-pics/index.vue
+++ src/components/eap-pics/index.vue
@@ -19,6 +19,14 @@ export default {
     $p() {
       return require('@img/' + this.url + '.png')
     } },
+  watch: {
+    list1: {
+      handler(n) {
+        this.$forceUpdate(n)
+      } }},
+  methods: {
+    methodThatForces() {
+    } },
   render: function(h) {
     return h('div', { style: { backgroundImage: 'url(' + this.$p + ')' }, attrs: { id: 'id' }}, this.list1.map((t, id) => h('div', { style: t }, t['id'])))
   }

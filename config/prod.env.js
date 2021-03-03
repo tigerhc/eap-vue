a23873d8f8357aa603c87cf1f0bdb1ff9b8c918e
hesiguo <siguo.he@totainfo.com>
2021-03-03 13:52:23 +0800

    新加路径

--- config/prod.env.js
+++ config/prod.env.js
@@ -4,4 +4,5 @@ module.exports = {
   BASE_API: '"http://pjad.com:800"'
   // BASE_API: '"http://10.160.144.8:800"'
   // BASE_API: '"http://172.16.10.111:800"'
+  // BASE_API: '"http://10.196.80.151:800"'
 }

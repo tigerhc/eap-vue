e55bb72dd64b15998d6d801df8641ddd6f2d90b6
jiafuxing <1640158339@qq.com>
2021-03-17 14:32:22 +0800

    量测分离

--- src/views/oven/sx/index.vue
+++ src/views/oven/sx/index.vue
@@ -1,8 +1,9 @@
 
 <template>
+  <div class="app-container calendar-list-container">
     <div class="condition-panel">
       <el-form class="form" label-width="90px" size="small">
-        <el-row :gutter="10">
+        <el-row>
           <el-col :span="4" >
             <el-form-item label="类型:">
               <el-select v-model="form1.type" class="wid90" @change="findProduction">
@@ -10,7 +11,7 @@
               </el-select>
             </el-form-item>
           </el-col>
-        <el-col :span="4">
+        <el-col :span="4" >
           <el-form-item label="机种名:">
             <div class="condition">
               <el-select v-model="form.productionName" class="wid90" >
@@ -24,55 +25,29 @@
           </el-form-item>
         </el-col>
           <el-col :span="4">
-          <el-form-item label="串行计数器:">
-            <el-select v-model="form.number" class="wid90" >
-              <el-option v-for="item in lineTypeResult" :key="item.value" :label="item.label" :value="item.value" />
-            </el-select>
-          </el-form-item>
-          </el-col>
-          <el-col :span="4">
-            <el-form-item label="位置:">
-              <el-select v-model="form.local" class="wid90" >
-                <el-option v-for="item in localResult" :key="item.value" :label="item.label" :value="item.value" />
-              </el-select>
+            <el-form-item label="位置">
+              <el-cascader
+                v-model="value"
+                :options="options"
+                :props="{ expandTrigger: 'hover' }"
+                @change="search"/>
             </el-form-item>
           </el-col>
-        <el-col :span="4">
+        <el-col :span="4" >
           <el-form-item label="日期:">
-            <el-date-picker v-model="dateTime" type="daterange" value-format="yyyy-MM-dd" range-separator="-" start-placeholder="开始日期" end-placeholder="结束日期" class="dateTimeClass"/>
+            <el-date-picker v-model="dateTime" style="width: 100%" type="daterange" value-format="yyyy-MM-dd" range-separator="-" start-placeholder="开始日期" end-placeholder="结束日期" class="dateTimeClass"/>
           </el-form-item>
         </el-col>
-          <el-col :span="4" :offset="-1">
-          <el-form-item >
-            <el-button type="primary" @click="search">查询</el-button>
-          </el-form-item>
-          </el-col>
+          <el-button type="primary" style="height: 32px ;margin-left: 5px" @click="search">查询</el-button>
         </el-row>
       </el-form>
+    </div>
       <div id="main" style="width: 80%;height: 500px;overflow: hidden;"/>
     </div>
 </template>
 <script>
 import echarts from 'echarts'
 import { productionName } from '@/api/oven/temperature'
-// import { findSxNumber } from '@/api/public'
-// import {
-//   TitleComponent,
-//   ToolboxComponent,
-//   TooltipComponent,
-//   GridComponent,
-//   LegendComponent
-// } from 'echarts/components'
-// import {
-//   LineChart
-// } from 'echarts/charts'
-// import {
-//   CanvasRenderer
-// } from 'echarts/renderers'
-//
-// echarts.use(
-//   [TitleComponent, ToolboxComponent, TooltipComponent, GridComponent, LegendComponent, LineChart, CanvasRenderer]
-// )
 import { findSxNumber } from '@/api/ms/monitor'
 export default {
   name: 'Index',
@@ -122,6 +97,39 @@ export default {
       }, {
         value: 'd',
         label: 'D'
+      }],
+      options: [{
+        value: '0001',
+        label: '1',
+        children: [{
+          value: 'a',
+          label: 'A'
+        }, {
+          value: 'b',
+          label: 'B'
+        }, {
+          value: 'c',
+          label: 'C'
+        }, {
+          value: 'd',
+          label: 'D'
+        }]
+      }, {
+        value: '0002',
+        label: '2',
+        children: [{
+          value: 'a',
+          label: 'A'
+        }, {
+          value: 'b',
+          label: 'B'
+        }, {
+          value: 'c',
+          label: 'C'
+        }, {
+          value: 'd',
+          label: 'D'
+        }]
       }]
     }
   },
@@ -132,6 +140,8 @@ export default {
       })
     },
     search() {
+      this.form.number = this.value[0]
+      this.form.local = this.value[1]
       this.form.type = this.form1.type
       this.form.startDate = this.dateTime[0]
       this.form.endDate = this.dateTime[1]
@@ -226,12 +236,19 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .condition-panel {
-    width: 100%;
-    height: 100%;
+  .Rtplotyieldday {
+    width: auto;
+    height: auto;
     margin: 0 auto;
-  .form {
-    margin-top: 20px;
-  }
+
+    .form {
+      margin-top: 20px;
+    }
+    .condition{
+      box-sizing: border-box;
+      display: inline-block;
+      font-family: sans-serif;
+      margin: 0;
+    }
   }
 </style>

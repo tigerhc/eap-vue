d59f527e46645decc9c7fd3f475a72364f083bb8
jiafuxing <1640158339@qq.com>
2021-03-26 15:12:23 +0800

    公告

---
+++ src/views/fab/notice/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="app-container calendar-list-container">
+
+    <w-table v-slot="{row}" v-bind="table" url="/oa/oanotification" sort="create_date.desc" >
+      <!--todo fixed属性导致当前列变为第一列-->
+      <w-table-col name="subject" label="标题" align="left" handler="view" query condition="like"/>
+      <w-table-col name="content" label="内容" width="400" align="left"/>
+      <w-table-col name="createBy" label="创建人" width="300" align="left" />
+      <w-table-col name="status" label="有效标志" width="150" align="center" dict="ACTIVE_FLAG" query condition="eq"/>
+      <w-table-col name="createDate" label="创建时间" sort width="200" align="center" query querymode="date" condition="between"/>
+
+      <w-table-toolbar name="add" url="views/fab/notice/indexEdit" />
+
+    </w-table>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data() {
+    return {
+      table: {
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/fab/notice/indexEdit.vue
@@ -0,0 +1,51 @@
+<template>
+    <w-form v-bind="formConf" :col="3" :model="model">
+      <el-input v-model="model.subject" align="center" label="标题" />
+      <el-input v-model="model.content" label="内容" />
+      <el-input v-model="model.createBy" label="更新人" />
+      <w-select-dic v-model="model.status" style="width:100%" label="有效标志" dict="ACTIVE_FLAG" />
+<!--      <el-input v-model="model.updateByName" :disabled="true" label="更新人" />-->
+<!--      <el-input v-model="model.updateDate" label="更新日期" />-->
+    </w-form>
+<!--  <div>2344444444444444444</div>-->
+</template>
+
+<script>
+export default {
+  name: 'SysFunctionEdit',
+  data() {
+    return {
+      model: {
+        subject: '',
+        content: '',
+        createBy: '',
+        status: ''
+      },
+
+      formConf: {
+        url: '/oa/oanotification/',
+        title: {
+          ADD: '新增功能',
+          EDIT: '修改功能',
+          VIEW: '功能详情'
+        },
+        rules: {
+          activeFlag: [{ required: true, message: '有效标志必选', trigger: 'change' }]
+        },
+        onLoadData: (m, type) => {
+          console.info(m)
+          return m
+        },
+        beforeSubmit: (params, type) => {
+          const re = { ...params }
+          return re
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

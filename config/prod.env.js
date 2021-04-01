aecdb97b6b94ba6f9c9513ad1736dca3eca48dc0
jiafuxing <1640158339@qq.com>
2021-04-01 08:35:55 +0800

    追加url

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,8 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
-  BASE_API: '"http://pjad.com:800"'
+  // BASE_API: '"http://pjad.com:800"'
   // BASE_API: '"http://10.160.144.8:800"'
+  BASE_API: '"http://10.160.144.9:800"'
   // BASE_API: '"http://172.16.10.111:800"'
 }

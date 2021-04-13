6dccb6ace06282dc2e414444fc5062e94ee47fbf
hesiguo <siguo.he@totainfo.com>
2021-04-13 10:27:29 +0800

    调整配方检验日志页面样式

--- src/views/rms/recipechecklog/rmsrecipechecklogList.vue
+++ src/views/rms/recipechecklog/rmsrecipechecklogList.vue
@@ -4,13 +4,11 @@
       <w-table-col name="eqpId" label="设备号" width="120" sort fixed align="left" handler="view" query eqp multiple namekey="id" condition="in"/>
       <w-table-col name="recipeCode" label="配方CODE" align="center" />
       <w-table-col name="recipeName" label="配方名" align="center" />
-      <w-table-col name="versionType" label="版本类型" align="center" />
-      <w-table-col name="versionNo" label="版本号" align="center" />
       <w-table-col name="paramCode" label="参数CODE" align="center" />
       <w-table-col name="paramName" label="参数名称" align="center" />
-      <w-table-col name="checkResult" label="校验结果" align="center" />
-      <w-table-col name="checkRemarks" label="校验信息" align="center" />
-      <w-table-col name="checkDate" label="校验时间" width="250" align="center" sort="1" query querymode="date" condition="between"/>
+      <w-table-col name="checkResult" label="校验结果" width="100" align="center" />
+      <w-table-col name="checkRemarks" label="校验信息" width="350" align="center" />
+      <w-table-col name="checkDate" label="校验时间" width="180" align="center" sort="1" query querymode="date" condition="between"/>
       <w-table-toolbar name="add" hidden/>
       <w-table-toolbar name="batchDelete" hidden />
     </w-table>

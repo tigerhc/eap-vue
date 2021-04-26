4feee1e311bb0aa311bd547d4ba5cb9919121a34
jiafuxing <1640158339@qq.com>
2021-04-26 09:07:59 +0800

    Revert "APJ 告警统计"
    This reverts commit 342ba11e0d2299c0a6060bb83231368db56898c5.

--- src/views/ams/record/alarmReport.vue
+++ src/views/ams/record/alarmReport.vue
@@ -63,8 +63,24 @@ export default {
       source2: [],
       // 先写死
       lineNoOptions: [{
-        value: 'APJ',
-        label: 'APJ'
+        value: 'SIM',
+        label: 'SIM'
+      }, {
+        value: 'SMA',
+        label: 'SMA',
+        disabled: true
+      }, {
+        value: 'SX',
+        label: 'SX',
+        disabled: true
+      }, {
+        value: '5GI',
+        label: '5GI',
+        disabled: true
+      }, {
+        value: '6GI',
+        label: '6GI',
+        disabled: true
       }]
     }
   },

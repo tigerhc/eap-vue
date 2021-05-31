0b6a52be2454162a2b95bb2dc856541c1250e3c4
DSK <wangdong>
2021-05-31 16:18:10 +0800

    SIM  站点是有换行的

--- src/views/fab/status/index.vue
+++ src/views/fab/status/index.vue
@@ -9,7 +9,7 @@
       </el-col>
       <el-col :span="16">
         <div>
-          <strong><span id="FirstTitle" style="font-size: 20px">SIM工程生产情报</span> </strong><date />
+          <strong><span id="FirstTitle" style="font-size: 20px">APJ工程生产情报</span> </strong><date />
         </div>
 
         <el-row>

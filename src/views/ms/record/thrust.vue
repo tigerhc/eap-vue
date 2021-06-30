683c69c440a06f0c2a641e1d1b28888874189e7e
DSK <wangdong>
2021-06-30 17:40:12 +0800

    推力拉力

---
+++ src/views/ms/record/thrust.vue
@@ -0,0 +1,419 @@
+<template>
+  <div class="app-container calendar-list-container">
+    <el-form class="form" label-width="90px" size="small">
+      <el-row>
+        <el-col :span="4">
+          <el-form-item>
+            <el-select :style="{width:'80px'}" v-model="form.lineNo" clearable placeholder="线别">
+              <el-option v-for="item in lines" :key="item" :label="item" :value="item" />
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="6">
+          <el-form-item>
+              <el-select v-model="form.eqpId" clearable placeholder="机种名">
+                <el-option v-for="item in thrustEqpId" :key="item.eqpId" :label="item.eqpName" :value="item.eqpId" />
+              </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="10">
+          <el-form-item prop="dateTime">
+            <el-date-picker
+              v-model="form.dateTime"
+              type="daterange"
+              value-format="yyyy-MM-dd"
+              range-separator="至"
+              start-placeholder="开始日期"
+              end-placeholder="结束日期"
+            />
+          </el-form-item>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="primary" @click="search">查询</el-button>
+          <el-button type="primary" @click="finddetail">导出</el-button>
+        </el-col>
+      </el-row>
+    </el-form>
+    <el-tabs v-model="editableTabsValue" type="card" >
+      <el-tab-pane v-for="item in editableTabs" :key="item.title" :label="item.title" :name="item.title" />
+    </el-tabs>
+    <div class="eqpTemp">
+      <div id="XtempChart" style="width: 100%; height: 280px; overflow: hidden" />
+    </div>
+    <div class="eqpTemp">
+      <div id="RtempChart" style="width: 100%; height: 280px; overflow: hidden; margin-top:100px" />
+    </div>
+  </div>
+</template>
+
+<script>
+import * as echarts from 'echarts'
+import api from '../../oven/ports/fetch'
+export default {
+  name: 'Tempchar',
+  data() {
+    return {
+      lines: ['5GI', '6GI'],
+      thrustEqpId: [],
+      api: api('/ms/measuresx/findSxNumberExport'),
+      toolbarStatus: {
+        exportsLoading: false
+      },
+      form: {
+        lineNo: '',
+        eqpId: undefined,
+        dateTime: []
+      },
+      editableTabsValue: '拉力',
+      editableTabs: [{ 'index': 0, 'title': '拉力' }, { 'index': 1, 'title': '推力' }],
+      echartData: [],
+      echartAxis: [],
+      echartCL: [],
+      echartUCL: [],
+      echartLCL: [],
+      echartRed: []
+    }
+  },
+  mounted() {
+    var now = new Date()
+    var endDate = new Date(Date.UTC(now.getFullYear(), now.getMonth(), now.getDate())).toISOString().slice(0, 10)
+    var startDate = endDate.slice(0, 8) + '01'
+    this.form.dateTime = [startDate, endDate]
+  },
+  created() {
+    // eqpList().then((response) => {
+    //   this.tempEqpId = response.data.eqpId
+    // })
+  },
+  methods: {
+    onValueChange(name) {
+      this.form.eqpId = name
+    },
+    search() {
+      this.echartData.push(2.8)
+      this.echartData.push(3.2)
+      this.echartData.push(5.6)
+      this.echartData.push(4.8)
+      this.echartData.push(4.6)
+      this.echartAxis.push('a')
+      this.echartAxis.push('b')
+      this.echartAxis.push('c')
+      this.echartAxis.push('d')
+      for (var i = 0; i < this.echartAxis.length; i++) {
+        this.echartCL.push(2)
+        this.echartUCL.push(4)
+        this.echartLCL.push(6)
+        this.echartRed.push(1)
+      }
+      var chartX = echarts.init(document.getElementById('XtempChart'))
+      chartX.setOption(this.getOption('X'))
+      var chartR = echarts.init(document.getElementById('RtempChart'))
+      chartR.setOption(this.getOption('R'))
+    },
+    getOption(XRflag) {
+      const Cureoption = {
+        title: {
+          text: ''
+        },
+        color: ['#60c947', '#efbe29', '#efbe29', '#ee1313'],
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#6a7985'
+            }
+          }
+        },
+        legend: {
+          data: this.charLegend
+          // data: ['运行温度', '低温报警', '高温报警']
+        },
+        dataZoom: [
+          {
+            type: 'inside',
+            start: 0,
+            end: 100
+          },
+          {
+            show: true,
+            type: 'slider',
+            y: '90%',
+            start: 0,
+            end: 100
+          }
+        ],
+        toolbox: {
+          show: true,
+          feature: {
+            dataZoom: {
+              yAxisIndex: 'none'
+            },
+            dataView: { readOnly: false },
+            magicType: { type: ['line', 'bar'] },
+            restore: {},
+            saveAsImage: {}
+          }
+        },
+        xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          axisLine: { onZero: false },
+          data: this.echartAxis
+        },
+        yAxis: { type: 'value' }
+      }
+      if (XRflag === 'X') {
+        Cureoption.title.text = 'X管理图'
+        Cureoption.series = this.getXseries()
+      } else if (XRflag === 'R') {
+        Cureoption.title.text = 'R管理图'
+        Cureoption.series = this.getRseries()
+      }
+      return Cureoption
+    },
+    getXseries() {
+      var series = [
+        {
+          name: '实测值',
+          itemStyle: {
+            normal: {
+              color: '#1a3eef'
+            }
+          },
+          type: 'line',
+          data: this.echartData,
+          symbol: 'diamond',
+          symbolSize: 20
+        },
+        {
+          name: 'X CL',
+          itemStyle: {
+            normal: {
+              lineStyle: {
+                color: '#60c947',
+                width: 2
+              }
+            }
+          },
+          type: 'line',
+          data: this.echartCL,
+          markLine: {
+            data: [{
+              name: '最大数据',
+              type: 'max'
+            }]
+          }
+        },
+        {
+          name: 'X UCL',
+          itemStyle: {
+            normal: {
+              lineStyle: {
+                color: '#efbe29',
+                width: 2
+              }
+            }
+          },
+          type: 'line',
+          data: this.echartUCL,
+          markLine: {
+            data: [{
+              name: '最大数据',
+              type: 'max'
+            }]
+          }
+        },
+        {
+          name: 'X LCL',
+          itemStyle: {
+            normal: {
+              lineStyle: {
+                color: '#efbe29',
+                width: 2
+              }
+            }
+          },
+          type: 'line',
+          data: this.echartLCL,
+          markLine: {
+            data: [{
+              name: '最大数据',
+              type: 'max'
+            }]
+          }
+        },
+        {
+          name: 'LCL',
+          itemStyle: {
+            normal: {
+              lineStyle: {
+                color: '#ee1313',
+                width: 2,
+                type: 'dashed'
+              }
+            }
+          },
+          type: 'line',
+          data: this.echartRed,
+          markLine: {
+            data: [{
+              name: '最大数据',
+              type: 'max'
+            }]
+          }
+        }
+      ]
+      return series
+    },
+    getRseries() {
+      var series = [
+        {
+          name: '偏差值',
+          itemStyle: {
+            normal: {
+              color: '#1a3eef',
+              lineStyle: {
+                color: '#1a3eef',
+                width: 3
+              }
+            }
+          },
+          type: 'line',
+          data: this.echartData,
+          symbol: 'diamond',
+          symbolSize: 20
+        },
+        {
+          name: 'R CL',
+          itemStyle: {
+            normal: {
+              lineStyle: {
+                color: '#60c947',
+                width: 2
+              }
+            }
+          },
+          type: 'line',
+          data: this.echartCL,
+          markLine: {
+            data: [{
+              name: '最大数据',
+              type: 'max'
+            }]
+          }
+        },
+        {
+          name: 'R UCL',
+          itemStyle: {
+            normal: {
+              lineStyle: {
+                color: '#efbe29',
+                width: 2
+              }
+            }
+          },
+          type: 'line',
+          data: this.echartUCL,
+          markLine: {
+            data: [{
+              name: '最大数据',
+              type: 'max'
+            }]
+          }
+        },
+        {
+          name: 'R LCL',
+          itemStyle: {
+            normal: {
+              lineStyle: {
+                color: '#efbe29',
+                width: 2
+              }
+            }
+          },
+          type: 'line',
+          data: this.echartLCL,
+          markLine: {
+            data: [{
+              name: '最大数据',
+              type: 'max'
+            }]
+          }
+        }
+      ]
+      return series
+    },
+    finddetail() {
+      // var param = this.$refs.ref.fdis()
+      // this.query.id = param
+      // eslint-disable-next-line no-undef
+      this.form.type = 'LF'
+      this.form.startDate = this.form.dateTime[0]
+      this.form.endDate = this.form.dateTime[1]
+      this.form.lineNo = 'SX68123M(AU)D-RP' // this.form.eqpId
+      if (this.toolbarStatus.exportsLoading) {
+        return
+      }
+      this.toolbarStatus.exportsLoading = true
+      // const q = (this.query)
+      // const q = this.form
+      var param = {}
+      param.endDate = '2021-06-30'
+      param.lineNo = 'SX'
+      param.local = 'a'
+      param.local56GI = ''
+      param.localSimC = ''
+      param.number = '0001'
+      param.productionName = 'SX68123M(AU)D-RP'
+      param.startDate = '2021-06-01'
+      param.type = 'LF'
+      // alert(q)
+      this.api.export(param).then((response) => {
+        if (response.code === 0) {
+          return import('../../oven/ports/Export2Excel').then((excel) => {
+            excel.export_byte_to_excel(response.bytes, response.title)
+            this.toolbarStatus.exportsLoading = false
+          })
+        } else {
+          this.$notify.error({
+            title: '失败',
+            message: (response && response.errmsg) || '导出失败!',
+            duration: 2000
+          })
+          this.toolbarStatus.exportsLoading = false
+        }
+      }).catch((e) => {
+        this.toolbarStatus.exportsLoading = false
+      })
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+  .tempchar {
+    width: 100%;
+    height: 100%;
+    margin: 0 auto;
+
+    .form {
+      margin-top: 20px;
+    }
+  }
+  #chartPanelLeft {
+    float: left;
+    width: 40%;
+    height: 100%;
+    margin-left: 0px;
+  }
+  #chartPanelRight {
+    float: left;
+    width: 40%;
+    height: 100%;
+  }
+  img {
+    width: 100%;
+    height: 100%;
+    border: 1px solid red;
+  }
+</style>

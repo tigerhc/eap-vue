f281ddf9b650f612d91571b8f39eab1c7bfc9b74
hesiguo <siguo.he@totainfo.com>
2021-08-06 09:17:25 +0800

    中试线需求修改，table组件加导出路径参数

--- src/views/fab/account/eqpAccountList.vue
+++ src/views/fab/account/eqpAccountList.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="app-container calendar-list-container">
     <w-table v-slot="{row}" v-bind="table" url="/ms/fabequipmentaccount" sort="date.desc" >
-      <w-table-col name="eqpId" label="设备号" width="120" sort fixed align="left" handler="view" query eqp multiple namekey="id" condition="in"/>
-      <w-table-col name="projectName" label="内容" align="center" />
+      <w-table-col name="eqpId" label="设备号" sort fixed align="center" handler="view" query eqp multiple namekey="id" condition="in"/>
+      <w-table-col name="eqpName" label="设备名称" align="left" />
+      <w-table-col name="projectName" label="内容" align="left" />
       <w-table-col name="lotNo" label="批次" align="center" />
       <w-table-col name="number" label="片数" align="center" />
-      <w-table-col name="startTime" label="开始时间" width="250" align="center" sort="1"/>
-      <w-table-col name="endTime" label="结束时间" width="250" align="center" sort="1"/>
-      <w-table-col name="date" label="日期" width="250" align="center" sort="1" query querymode="date" condition="between"/>
-      <w-table-col name="person" label="操作员" align="center" />
+      <w-table-col name="startTime" label="开始时间" align="center" sort="1"/>
+      <w-table-col name="endTime" label="结束时间" align="center" sort="1"/>
+      <w-table-col name="date" label="日期" align="center" sort="1" query querymode="date" condition="between"/>
+      <w-table-col name="person" label="操作员" align="center" sort />
       <w-table-toolbar name="add" hidden/>
       <w-table-toolbar name="batchDelete" hidden />
     </w-table>
